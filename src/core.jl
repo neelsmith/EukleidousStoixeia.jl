9869d4932899752f12878cf24e1bc5d7da0210be
Neel Smith <dnsmith.neel@gmail.com>
2024-01-15 04:53:27 -0500

    Expand comments

--- src/core.jl
+++ src/core.jl
@@ -1,10 +1,19 @@
 
 """Return a Makie figure for a CtsUrn."""
 function diagram(u::CtsUrn; fig = Figure())
-    # Add axis if none in figure!
+    # Add axis if none in figure:
     if isempty(fig.content)
         Axis(fig[1,1], aspect=DataAspect(), limits = (-1, 1, -1, 1))
     end
+
+    #= Based on URN, delegate to correct method:
+
+    - need to find right work (for now, just Euc.Elem.)
+    - determine both proposition and part (protasis, kataskeuh, apodeixis)
+    
+    =#
+
+
     fig
 end
 
